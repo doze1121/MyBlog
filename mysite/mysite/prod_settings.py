6d155fd8ee8585d56a2747fc3b6e6e1e04f427cd
doze1121 <doze1121@gmail.com>
2023-10-17 11:59:00 +0300

    fix

--- mysite/mysite/prod_settings.py
+++ mysite/mysite/prod_settings.py
@@ -14,7 +14,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'blog',
-        'USER': 'postgres',
+        'USER': 'userdb',
         'PASSWORD': '123456',
         'HOST': 'localhost',
         'PORT': '5432',
